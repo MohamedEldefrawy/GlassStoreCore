e9e6d66e6d665119cfd061a5db2104f04716a1cf
Mohamed Eldefrawy <Mohamed.Eldefrawy13026011@aiet.edu.eg>
2020-09-14 21:11:00 +0200

    Implement unit of work pattern

---
+++ GlassStoreCore/BL/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GlassStoreCore.BL.Repositories;
+
+namespace GlassStoreCore.BL
+{
+    interface IUnitOfWork
+    {
+        IUsersRepository UsersRepository { get; }
+        Task<int> Complete();
+    }
+}
---
+++ GlassStoreCore/BL/Repositories/IUsersRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GlassStoreCore.BL.Models;
+
+namespace GlassStoreCore.BL.Repositories
+{
+    public interface IUsersRepository
+    {
+        void Update(ApplicationUser user);
+
+    }
+}
