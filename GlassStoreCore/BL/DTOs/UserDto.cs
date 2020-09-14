97e296a20059377c6f5f1faf4af04a063c5c307e
Mohamed Eldefrawy <Mohamed.Eldefrawy13026011@aiet.edu.eg>
2020-09-14 21:21:05 +0200

    Create Users Data transfer objects classes

---
+++ GlassStoreCore/BL/DTOs/UserDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GlassStoreCore.BL.DTOs
+{
+    public class UserDto
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public ICollection<UserRoleDto> Roles { get; set; }
+    }
+}
---
+++ GlassStoreCore/BL/DTOs/UserRoleDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GlassStoreCore.BL.DTOs
+{
+    public class UserRoleDto
+    {
+        public string UserId { get; set; }
+        public string RoleId { get; set; }
+        public string RoleName { get; set; }
+    }
+}
