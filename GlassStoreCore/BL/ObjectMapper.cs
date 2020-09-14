3b551bd9edcc07461bbed8052b50c9dfb33fe01c
Mohamed Eldefrawy <Mohamed.Eldefrawy13026011@aiet.edu.eg>
2020-09-14 21:28:00 +0200

    implement AutoMapper

---
+++ GlassStoreCore/BL/ObjectMapper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using GlassStoreCore.BL.DTOs;
+using GlassStoreCore.BL.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace GlassStoreCore.BL
+{
+    public class ObjectMapper
+    {
+        public IMapper Mapper { get; set; }
+
+        public ObjectMapper()
+        {
+            var config = new MapperConfiguration(cfg =>
+                                                 {
+                                                     cfg.CreateMap<ApplicationUser, CreateUserDto>();
+                                                     cfg.CreateMap<CreateUserDto, ApplicationUser>();
+                                                     cfg.CreateMap<ApplicationUser, UserDto>();
+                                                     cfg.CreateMap<UserDto, ApplicationUser>();
+                                                     cfg.CreateMap<IdentityUserRole<string>, UserRoleDto>();
+                                                     cfg.CreateMap<UserRoleDto, IdentityUserRole<string>>();
+                                                 });
+
+            this.Mapper = config.CreateMapper();
+
+        }
+    }
+
+}
