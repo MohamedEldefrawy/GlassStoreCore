475db440c65d8a40e2412c5bcb86edccca5ad5db
Mohamed Eldefrawy <Mohamed.Eldefrawy13026011@aiet.edu.eg>
2020-10-15 00:00:18 +0200

    create wholeSaleProducts orders dtos

---
+++ GlassStoreCore/BL/DTOs/WholeSaleProductsDtos/WholeSaleProductsOrderDetails.cs
@@ -0,0 +1,14 @@
+﻿using GlassStoreCore.BL.Models;
+
+namespace GlassStoreCore.BL.DTOs.WholeSaleProductsDtos
+{
+    public class WholeSaleProductsOrderDetails
+    {
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public WholeSaleProductsDto WholeSaleProduct { get; set; }
+        public WholeSaleSellingOrdersDto WholeSaleSellingOrder { get; set; }
+
+
+    }
+}
---
+++ GlassStoreCore/BL/DTOs/WholeSaleProductsDtos/WholeSaleSellingOrdersDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using GlassStoreCore.BL.Models;
+
+namespace GlassStoreCore.BL.DTOs.WholeSaleProductsDtos
+{
+    public class WholeSaleSellingOrdersDto
+    {
+        public DateTime OrderDate { get; set; }
+        public string UserId { get; set; }
+        public ICollection<WholeSaleSellingOrderDetail> WholeSaleSellingOrderDetails { get; set; }
+    }
+}
