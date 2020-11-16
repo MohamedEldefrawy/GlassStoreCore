5d692cfe9dcf69b20980f124e91f352091089735
Mohamed Eldefrawy <Mohamed.Eldefrawy13026011@aiet.edu.eg>
2020-11-16 21:16:00 +0200

    Create GetAll wholeSale products selling orders Api request

---
+++ GlassStoreCore/BL/APIs/WholeSaleSellingOrdersController.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using GlassStoreCore.BL.APIs.Filters;
+using GlassStoreCore.BL.DTOs.WholeSaleProductsDtos;
+using GlassStoreCore.BL.Models;
+using GlassStoreCore.Helpers;
+using GlassStoreCore.Services.PaginationUowService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GlassStoreCore.BL.APIs
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class WholeSaleSellingOrdersController : ControllerBase
+    {
+        private readonly ObjectMapper _mapper;
+        private readonly IPaginationUow _paginationUow;
+
+        public WholeSaleSellingOrdersController(ObjectMapper mapper, IPaginationUow paginationUow)
+        {
+            _mapper = mapper;
+            _paginationUow = paginationUow;
+        }
+
+        [HttpGet]
+        public ActionResult<WholeSaleSellingOrder> GetWholeSaleSellingOrders([FromQuery] PaginationFilter filter)
+        {
+            var route = Request.Path.Value;
+            var (orders, totalRecords) = _paginationUow.Service<WholeSaleSellingOrder>()
+                                                       .GetAll(filter.PageNumber, filter.PageSize).Result;
+
+            if (orders.Count == 0)
+            {
+                return NotFound("No Orders Available");
+            }
+
+            var ordersDto = orders.Select(_mapper.Mapper.Map<WholeSaleSellingOrder, WholeSaleSellingOrdersDto>)
+                                  .ToList();
+            var pageResponse =
+                PaginationHelper.CreatePagedResponse(ordersDto, filter, totalRecords, _paginationUow, route);
+
+            return Ok(pageResponse);
+        }
+    }
+}
